775868697b0cdc044030cb36d2676fe463a24c54
Jaruwit Sriburanasorn <Jaruwit@Jaruwits-MacBook-Air.local>
2017-11-09 20:11:52 +0700

    initial commit

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyC1mwsvVvwK1bJ3ku3DXVOzt-13WWt1e_Q",
+  authDomain: "myisproject-61270.firebaseapp.com",
+  databaseURL: "https://myisproject-61270.firebaseio.com/",
+  projectId: "myisproject-61270",
+  storageBucket: "myisproject-61270.appspot.com",
+  messagingSenderId: "767412484620"
+};
