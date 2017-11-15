27bc41a17e1c97aa91fd79c5ccdc86e0184b7899
Jaruwit Sriburanasorn <Jaruwit@Jaruwits-MacBook-Air.local>
2017-11-15 07:58:05 +0700

    report table

--- public/report.js
+++ public/report.js
@@ -2,12 +2,7 @@
  * Created by Jaruwit on 11/13/2017 AD.
  */
 
-// <select class="form-control" id="select">
-//     <option>กรุณาเลือกรายงาน</option>
-//     <option>จำนวนผู้ใช้งาน</option>
-//     <option>ใบรับประกันสินค้า</option>
-//     <option>สินค้าที่ส่งซ่อม</option>
-//     </select>
+database = firebase.database();
 
 function onReportCategoryChange() {
 
@@ -32,13 +27,17 @@ function onReportCategoryChange() {
         }
 
         //Create month select options
-        var monthOptions = ["รวมทั้งปี", "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
+        var monthOptions = ["รวมทั้งปี", "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
         var monthSelect = document.createElement("select");
         monthSelect.id = "monthSelect";
         div.insertBefore(monthSelect, div.children[2]);
         for (var i=0; i<monthOptions.length; i++) {
             var option = document.createElement("option");
-            option.value = monthOptions[i];
+            if (monthOptions == "รวมทั้งปี") {
+                option.value = 0;
+            } else {
+                option.value = i;
+            }
             option.text = monthOptions[i];
             monthSelect.appendChild(option);
         }
@@ -56,10 +55,85 @@ function onOKClick() {
     var element = document.getElementById("select");
     if (element.options[element.selectedIndex].text == "ใบรับประกันสินค้า") {
         //gen report
-        
+        database.ref('Warranties').once('value').then(function (snapshot) {
+            var dictionary = {};
+            var sum = 0;
+            snapshot.forEach(function (childSnapshot) {
+                childData = childSnapshot.val();
+                var buyDate = childData.buyDate;
+
+                var yearSelect = document.getElementById("yearSelect");
+                var monthSelect = document.getElementById("monthSelect");
+
+                var year = buyDate.split('/')[2];
+                var month = buyDate.split('/')[1];
+
+                if (year == yearSelect.options[yearSelect.selectedIndex].text
+                    && 0 == monthSelect.options[monthSelect.selectedIndex].value) {
+                    typeof dictionary[childData.type] === 'undefined' ? dictionary[childData.type] = 1 : dictionary[childData.type]++;
+                    sum++;
+                } else if (
+                    year == yearSelect.options[yearSelect.selectedIndex].text
+                    && month == monthSelect.options[monthSelect.selectedIndex].value) {
+                    typeof dictionary[childData.type] === 'undefined' ? dictionary[childData.type] = 1 : dictionary[childData.type]++;
+                    sum++;
+                }
+
+            });
+            generateTable(ReportType.Warranty, dictionary, sum);
+            console.log(dictionary);
+            console.log(sum);
+        });
     } else if (element.options[element.selectedIndex].text == "สินค้าที่ส่งซ่อม") {
         //
     } else {
 
+    }
+}
+
+var ReportType = {
+    Warranty: 1,
+    History: 2,
+    UserCount: 3
+}
+
+function generateTable(reportType, dictionary, sum) {
+    if (reportType == ReportType.Warranty) {
+        var tableSection = document.getElementById('table-section');
+        tableSection.innerHTML = "";
+        var tableTitle = document.createTextNode("รายงานประเภทสินค้าที่มีการส่งใบรับประกันเดือนธันวาคม");
+        tableSection.appendChild(tableTitle);
+
+        var table = document.createElement('TABLE');
+
+        var tableBody = document.createElement('TBODY');
+        table.appendChild(tableBody);
+
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+        var th1 = document.createElement('TH');
+        th1.width = '100';
+        th1.appendChild(document.createTextNode("ประเภทสินค้า"));
+        tr.appendChild(th1);
+
+        for (var i = 0; i < Object.keys(dictionary).length; i++) {
+            var tr = document.createElement('TR');
+            tableBody.appendChild(tr);
+
+            for (var j = 0; j < 3; j++) {
+                var td = document.createElement('TD');
+                td.width = '100';
+                if (j == 0) {
+                    td.appendChild(document.createTextNode(Object.keys(dictionary)[i]));
+                } else if (j == 1) {
+                    td.appendChild(document.createTextNode(dictionary[Object.keys(dictionary)[i]]))
+                } else {
+                    td.appendChild(document.createTextNode("เครื่อง "));
+                }
+
+                tr.appendChild(td);
+            }
+        }
+        tableSection.appendChild(table);
     }
 }
